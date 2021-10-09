1f1d501f1d42c1a5deb7ba5230ab1f7f5c9e2251
notin <panGit1!>
2021-10-09 13:59:46 -0700

    second commit

---
+++ src/component/leftHandNav.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./leftHandNav.scss"
+
+class leftHandNav extends Component<any, any> {
+
+    state = {}
+
+    render() {
+        var nav = <div id = "pokemonNav"></div>
+        return nav;
+    }
+
+}
+export default leftHandNav;
