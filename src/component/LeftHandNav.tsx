7c6db146d3e87d08f69d20619075c7761d899173
notin <panGit1!>
2021-10-09 22:37:59 -0700

    making list work

--- src/component/LeftHandNav.tsx
+++ src/component/LeftHandNav.tsx
@@ -5,12 +5,31 @@ import {Simulate} from "react-dom/test-utils";
 
 function LeftHandNav() {
     useEffect(()=> {fetchItems();},[])
-    const [items, setItems] = useState([])
+    let [items, setItems] = useState([])
 
     const fetchItems= async ()=> {
         const data = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        const items = await data.json();
+        setItems(items.results);
     }
 
-    return (<div id="pokemonNav" style={{width: "25%"}}>Pokemon</div>)
+
+    function getLi(item:any) {
+        const li = <li>
+                        <div className="listItems" style={{width: "25%"}}>
+                            {item.name}
+                        </div>
+                    </li>;
+        return li;
+    }
+
+    let div = <>
+        <div id="pokemonNav" style={{width: "25%"}}>Pokemon</div>
+        <ul>
+
+            {items.map(item =>getLi(item))}
+        </ul>
+    </>;
+    return div
 }
 export default LeftHandNav;
