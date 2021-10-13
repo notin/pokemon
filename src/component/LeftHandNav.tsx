5a724e7ead8ff5b61758d4b19e0488f6e98d0c6f
notin <panGit1!>
2021-10-13 09:55:58 -0700

    abilities displaying now

--- src/component/LeftHandNav.tsx
+++ src/component/LeftHandNav.tsx
@@ -4,19 +4,19 @@ import {BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Pokemon from "./pokemon/Pokemon";
 
 function LeftHandNav() {
-    useEffect(()=> {fetchItems();},[])
+    useEffect(()=> {fetchItems();},[]);
+    // @ts-ignore
+    let url = "https://pokeapi.co/api/v2/pokemon/";
     let [items, setItems] = useState([])
 
-    let index =0
     let fetchItems= async ()=> {
-        let data = await fetch("https://pokeapi.co/api/v2/pokemon/");
+        let data = await fetch(url);
         let items = await data.json();
         setItems(items.results);
     }
 
 
     function getLi(item:any, id:number) {
-
         let li =
             <Link id={item.name + "-" + id}
                   to={{pathname :"/components/pokemon/Pokemon",
--- src/component/pokemon/Pokemon.tsx
+++ src/component/pokemon/Pokemon.tsx
@@ -1,47 +1,56 @@
 import React, {Component, useState, useEffect } from "react";
+
 import {BrowserRouter as Router, Link, Route, useLocation} from "react-router-dom";
-// import "./LeftHandNav.scss"
+
 
 let Pokemon = () => {
-
-    console.log("Pokemon was clicked")
-    useEffect(()=> {fetchProfile();},[])
-    let [pokemon, setPokemon] = useState([])
+    useEffect(()=> {fetchItems().then(r =>
+        console.log("got pokemon details"))
+    ;},[])
     let props = useLocation();
+
     // @ts-ignore
     let url = props.state.item.url;
+    let [pokemon, setPokemon] = useState([]);
 
-    let fetchProfile = async () => {
-        let pokeProfile = await fetch(url);
-        let info = await pokeProfile.json();
-        setPokemon(info);
-    }
 
-    function getAbility(ability: any ) {
-        let li =
-                <li className="listItems">
-                    <div className="listItems">
-                        {ability.name}
-                    </div>
-                </li>
-
-        return li;
+    let fetchItems = async () => {
+        let data = await fetch(url);
+        let items = await data.json();
+        setPokemon(items);
     }
 
 // @ts-ignore
+    let getAbilities =(ability)=> {
+        let a =
+            <div className="listItems">
+                {ability.ability.name}
+            </div>
+        return a;
+    }
+    // @ts-ignore
+    let getAbiltiesFromPokeom = (poke) => {
+        let abilities = poke.pokemon.abilities;
+        console.log(abilities);
+        let li =<li className="listItems">
+            {(abilities|| []).map((a : any)=>getAbilities(a))}
 
+        </li>
+        return li;
+    };
+    // @ts-ignore
+    let p = <>{props.state.item.name}</>;
     let div = <div className="displayInblock">
 
-            <nav id="a" className="listTitle">
-                <ul className="listItems">
-                    <div>
-                        <p>Pokemon</p>
-                    </div>
-                    {/*<div>{pokemon}</div>*/}
-
-                </ul>
-            </nav>
+        <nav id="a" className="listTitle">
+            <div>
+                <p>{p}</p>
+            </div>
+            <ul className="listItems">
+                {getAbiltiesFromPokeom({pokemon})}
+            </ul>
+        </nav>
     </div>;
-    return div;
+    return div
 };
 export default Pokemon;
