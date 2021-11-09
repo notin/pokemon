362b47265bba1ee5c2b2c933b562078a92716639
notin <panGit1!>
2021-11-09 11:04:21 -0800

    refreshing list properly

--- src/component/move/Move.tsx
+++ src/component/move/Move.tsx
@@ -5,6 +5,7 @@ import {PokemonContext} from "../pokemon/Pokemon";
 import MoveType from "./MoveType";
 import "./Move.scss"
 import Collapsible from "react-collapsible";
+import pk from "../../contexts/pk";
 
 interface IndexProp {
     index: number;
@@ -15,7 +16,7 @@ let Move = ( index: IndexProp ) => {
     let props = useLocation();
     // @ts-ignore
     let url = props.state.item.url;
-    let [move, setMoves] = useState<MoveType>();
+    let [move, setMoves] = useState<MoveType>(url);
     let context = useContext(PokemonContext);
     
     useEffect(()=> {fetchMove().then(() =>
@@ -29,23 +30,36 @@ let Move = ( index: IndexProp ) => {
 
         let type = items.type.name
         let name = items.name;
-        let moveType = new MoveType(name, type, "", items.accuracy);
-        setMoves(moveType);
-    }
+        let url = items.url;
 
+        // @ts-ignore
+        let find = items.learned_by_pokemon.find(x=>x.name == pk.pokeName);
+        if(find){
+            let moveType = new MoveType(name, type, "", items.accuracy);
+            setMoves(moveType);
+        }
+        else {
+            let message = "move not associated with pokemon : " + name;
+            console.log(message);
+            let elementById = document.getElementById(name);
+            if(elementById) {
+                message = "removing move not associated with pokemon : " + name;
+                console.log(message);
+                elementById.remove();
+            }
+        }
+    }
 
     function getMove() {
         let element = null;
         // @ts-ignore
-        if(move != undefined){
-            element = <div>
+        if(move != undefined && move.name != undefined){
+            element = <div id = {move.name}>
                 <p>
-                    <>Move : </>
-                    <Collapsible trigger={move.name}>
+                    <Collapsible trigger={ "Move: "+ move.name}>
                         <div>type : {move.type}</div>
                         <div>accuracy : {move.accuracy}</div>
                     </Collapsible>
-
                 </p>
             </div>;
         }
